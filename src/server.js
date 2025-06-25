10d4edc984bc7568a46917e8960c83daf3c237ab
Luan Henrique Da Silva Bonfim <luan.bonfim@fbetterbeef.local>
2025-06-24 22:55:25 -0300

    a

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const app = require("./app");
+const { exec } = require("child_process");
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+console.log("Rodando migrações do Prisma...");
+
+exec("npx prisma migrate deploy", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Erro ao rodar migrações: ${error.message}`);
+    process.exit(1); // encerra o app se não conseguir rodar migração
+  }
+  if (stderr) {
+    console.error(`Erro: ${stderr}`);
+  }
+  console.log(stdout);
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
 });
